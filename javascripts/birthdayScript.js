a75630e777e806047d5cb2387bafea094c4245b0
Ryan Dickey <ryantay87@gmail.com>
2015-10-08 21:14:28 -0500

    added new javascripts folder and birthdayScript javascript file

---
+++ javascripts/birthdayScript.js
@@ -0,0 +1,5 @@
+var introMsg = function() {
+	var message = 'Welcome to SAO Xgen.\n\nToday is special. You have purchased your new NERVE Gear from your local store.Very trusting of you to come back. What do you have to prove, Kirito?\n\n Anyway, shall we begin?'
+
+	confirm(message);
+}

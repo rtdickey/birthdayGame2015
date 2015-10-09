a6f92bb1c5d14348288a3e3501b546f556f49e37
Ryan Dickey <ryantay87@gmail.com>
2015-10-08 22:11:10 -0500

    added more popups to begin the adventure

--- javascripts/birthdayScript.js
+++ javascripts/birthdayScript.js
@@ -1,5 +1,45 @@
 var introMsg = function() {
-	var message = 'Welcome to SAO Xgen.\n\nToday is special. You have purchased your new NERVE Gear from your local store.Very trusting of you to come back. What do you have to prove, Kirito?\n\n Anyway, shall we begin?'
+	var message = 'Welcome to SAO Xgen.\n\nToday is special. You have purchased your new NERVE Gear from your local store. Very trusting of you to come back. What do you have to prove, Kirito?\n\n Anyway, shall we begin?'
 
-	confirm(message);
+	var answer = confirm(message);
+	if(answer) {
+		rulesFromIntro();
+	} else {
+		introLeavingMsg();
+	}
+}
+
+var introLeavingMsg = function() {
+	var message = "So soon?\n\nYou didn't think it would be that easy did you? Shall we proceed?";
+
+	var answer = confirm(message);
+	if(answer) {
+		rules();
+	} else {
+		introLeaveAgain();
+	}
+}
+
+var introLeaveAgain = function() {
+	var message = "You don't learn very quickly do you?"
+	
+	var answer = prompt(message);
+}
+
+var rulesFromIntro = function() {
+	var message = "Let's go over a few rules...\n\n1) You will be have to make passed three bosses in order to receive the treasure you seek.\n\n2) You will level up along the way. The path you choose is yours. Make wise decisions\n\n3) Remember to have fun...\n\nMuahhhahahahahaaaaa!"
+	
+	var answer = confirm(message);
+
+	if(answer) {
+		//todo
+	} else {
+		rulesFromIntro();
+	}
+}
+
+var rules = function() {
+	var message = "I knew you would come around. Let's begin..."
+
+	var answer = prompt(message);
 }

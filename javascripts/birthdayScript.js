0b8a677bc3d5bf736d09ef0a16daeaf72f6dc275
Ryan Dickey <ryantay87@gmail.com>
2015-10-09 22:14:03 -0500

    added more functionality to the adventure. need to move on with character build

--- javascripts/birthdayScript.js
+++ javascripts/birthdayScript.js
@@ -16,18 +16,12 @@ var introLeavingMsg = function() {
 	if(answer) {
 		rules();
 	} else {
-		introLeaveAgain();
+		characterBuild();
 	}
 }
 
-var introLeaveAgain = function() {
-	var message = "You don't learn very quickly do you?"
-	
-	var answer = prompt(message);
-}
-
 var rulesFromIntro = function() {
-	var message = "Let's go over a few rules...\n\n1) You will be have to make passed three bosses in order to receive the treasure you seek.\n\n2) You will level up along the way. The path you choose is yours. Make wise decisions\n\n3) Remember to have fun...\n\nMuahhhahahahahaaaaa!"
+	var message = "Let's go over a few rules...\n\n1) You will have to make passed three bosses in order to receive the treasure you seek.\n\n2) You will level up along the way. The path you choose is yours. Make wise decisions\n\n3) Remember to have fun...\n\nMuahhhahahahahaaaaa!"
 	
 	var answer = confirm(message);
 
@@ -41,5 +35,31 @@ var rulesFromIntro = function() {
 var rules = function() {
 	var message = "I knew you would come around. Let's begin..."
 
-	var answer = prompt(message);
+	var answer = confirm(message);
+	if(answer) {
+		rulesFromIntro();
+	} else {
+		rules();
+	}
+}
+
+var characterBuild = function() {
+	var message = "You don't learn very quickly do you?\n\nOh well, let's begin. What class would you like to be this time?\n\n*Note: SAO Xgen is still in beta. There are only 2 classes.\n\nPlease type your selection:\n\nwarrior or mage?"
+	
+	var answer = confirm(message);
+	if(answer) {
+		characterDecision('warrior');
+	} else {
+		characterDecision('mage')
+	}
+}
+
+var characterDecision = function(message) {
+	var answer = confirm('You chose ' + message + '. Is this correct?');
+
+	if(answer) {
+		alert('good');
+	} else {
+		characterBuild();
+	}
 }
